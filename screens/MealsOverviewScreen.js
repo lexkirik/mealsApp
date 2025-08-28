07e73b25fba334a680ad0ce2519f378eda122bf4
Aliaksei Kirykovich <lexkirik@mail.ru>
2025-08-28 21:59:14 +0300

    added screens, navigation

---
+++ screens/MealsOverviewScreen.js
@@ -0,0 +1,34 @@
+import { Text, View, StyleSheet, FlatList } from "react-native";
+import { MEALS } from "../data/dummy-data";
+import MealItem from "../components/MealItem";
+
+function MealsOverviewScreen({ route }) {
+  const catID = route.params.categoryID;
+
+  const displayedMeals = MEALS.filter((mealItem) => {
+    return mealItem.categoryIds.indexOf(catID) >= 0;
+  });
+
+  function renderMealItem(itemData) {
+    return <MealItem title={itemData.item.title} />;
+  }
+
+  return (
+    <View style={styles.container}>
+      <FlatList
+        data={displayedMeals}
+        keyExtractor={(item) => item.id}
+        renderItem={renderMealItem}
+      />
+    </View>
+  );
+}
+
+export default MealsOverviewScreen;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 16,
+  },
+});
